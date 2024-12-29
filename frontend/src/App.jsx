26d933372d261d61e0e72f5eff6fe04dc4c14f3d
sangeeta8642 <sangeetaperagani8642@gmail.com>
2024-12-29 11:50:27 +0530

    initializing the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Button variant="default" >Click me</Button>
+    </div>
+  )
+}
+
+export default App
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
